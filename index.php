04e94fc2051a86a947500c2bf9aec1f2609b5670
lonelykatana <erickgultom325@gmail.com>
2020-06-11 17:54:38 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="eng">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <meta http-equiv="X-UA-Compatible" content="ie=encoding">
+        <title>Test Git Remote</title>
+    </head>
+    <body>
+        <h3>Hello World?</h3>
+    </body>
+</html>
